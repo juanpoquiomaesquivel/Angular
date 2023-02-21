a875bfd93161772217e7f2f05d47fd2a597bdbde
juanpoquiomaesquivel <juanpoquiomaesquivel@hotmail.com>
2023-02-21 16:07:24 -0500

    Curso Angular - Video 5 & 6 - Componentes
    -ng g c component_name

---
+++ pildorasinformaticas/miprimeraApp/src/app/empleado/empleado.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empleado',
+  templateUrl: './empleado.component.html',
+  // template: "<p>Aquí iría un empleado INLINE</p>",
+  styleUrls: ['./empleado.component.css']
+  // styles: ["p{background-color: blue;}"]
+
+  /**
+   * Para crear un componente INLINE:
+   * ng g c empleadoTirar -s -t
+   */
+})
+export class EmpleadoComponent {
+
+}
