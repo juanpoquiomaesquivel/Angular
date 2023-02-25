f37168e0c8cd69bd3df6992cf59beb3d9c84072d
juanpoquiomaesquivel <juanpoquiomaesquivel@hotmail.com>
2023-02-25 17:49:13 -0500

    Curso Angular - Video 7 & 8 - Interpolación
    -texto dinámico

--- pildorasinformaticas/miprimeraApp/src/app/empleado/empleado.component.ts
+++ pildorasinformaticas/miprimeraApp/src/app/empleado/empleado.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-empleado',
@@ -14,4 +14,16 @@ import { Component } from '@angular/core';
 })
 export class EmpleadoComponent {
 
+  nombre="Juan";
+  apellido="Diaz";
+  /*private */edad=18;
+  //empresa="Píldoras Informáticas";
+
+  /*getEdad() {
+    return this.edad;
+  }*/
+
+  /*llamaEmpresa(value:String) {
+
+  }*/
 }
