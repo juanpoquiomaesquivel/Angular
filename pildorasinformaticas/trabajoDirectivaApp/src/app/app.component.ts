70f3c1233f4cbd7b67c7677911c3f53a4e864356
juanpoquiomaesquivel <juanpoquiomaesquivel@hotmail.com>
2023-03-24 16:33:55 -0500

    Curso Angular - Video 14 - Directivas I: ngIf

---
+++ pildorasinformaticas/trabajoDirectivaApp/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  titulo = 'Registro de usuarios';
+  mensaje = "";
+  registrado = false;
+  nombre:string="";
+  apellido:string="";
+
+  registrarUsuario() {
+    this.registrado = true;
+    this.mensaje = "Usuario registrado con éxito"
+  }
+}
