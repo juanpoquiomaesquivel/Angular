5c86a27d101cc8df25e4788bf6ed84215102cb7e
juanpoquiomaesquivel <juanpoquiomaesquivel@hotmail.com>
2023-03-24 22:23:21 -0500

    Angular - 16 & 17 - Directivas III-IV: Práctica

---
+++ pildorasinformaticas/app-empleados/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { Empleado } from './empleado.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  titulo = 'Listado de empleados';
+
+  empleados:Empleado[]=[
+    new Empleado("Juan", "Díaz", "Presidente", 7500),
+    new Empleado("Ana", "Martín", "Directora", 5500),
+    new Empleado("María", "Fdez", "Jefe sección", 3500),
+    new Empleado("Laura", "López", "Administrativo", 2500),
+  ];
+
+  cuadroNombre:string="";
+  cuadroApellido:string="";
+  cuadroCargo:string="";
+  cuadroSalario:number=0;
+
+  agregarEmpleado() {
+    let miEmpleado=new Empleado(this.cuadroNombre, this.cuadroApellido, this.cuadroCargo, this.cuadroSalario);
+
+    this.empleados.push(miEmpleado);
+  }
+}
