f7969fd36af924695ba62dd04ad537c840d162b6
juanpoquiomaesquivel <juanpoquiomaesquivel@hotmail.com>
2023-03-31 07:16:12 -0500

    Angular - 24 & 25 - Routing

--- pildorasinformaticas/app-empleados/src/app/app.component.ts
+++ pildorasinformaticas/app-empleados/src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Empleado } from './empleado.model';
-import { EmpleadosService } from './empleados.service';
-import { ServicioEmpleadosService } from './servicio-empleados.service';
 
 @Component({
   selector: 'app-root',
@@ -9,29 +6,7 @@ import { ServicioEmpleadosService } from './servicio-empleados.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  titulo = 'Listado de empleados';
-  
-  constructor(private miServicio:ServicioEmpleadosService, private empleadosService:EmpleadosService) {
-    // this.empleados= this.empleadosService.empleados;
-
-  }
-
   ngOnInit(): void {
-    this.empleados= this.empleadosService.empleados;
   }
 
-  empleados:Empleado[]=[];
-
-  cuadroNombre:string="";
-  cuadroApellido:string="";
-  cuadroCargo:string="";
-  cuadroSalario:number=0;
-
-  agregarEmpleado() {
-    let miEmpleado=new Empleado(this.cuadroNombre, this.cuadroApellido, this.cuadroCargo, this.cuadroSalario);
-
-    //this.miServicio.muestraMensaje("Nombre del empleado" + miEmpleado.nombre);
-
-    this.empleadosService.agregarEmpleadoServicio(miEmpleado);
-  }
 }
---
+++ pildorasinformaticas/app-empleados/src/app/home-component/home-component.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { Empleado } from '../empleado.model';
+import { EmpleadosService } from '../empleados.service';
+import { ServicioEmpleadosService } from '../servicio-empleados.service';
+
+@Component({
+  selector: 'app-home-component',
+  templateUrl: './home-component.component.html',
+  styleUrls: ['./home-component.component.css']
+})
+export class HomeComponentComponent {
+
+  titulo = 'Listado de empleados';
+
+  constructor(private miServicio:ServicioEmpleadosService, private empleadosService:EmpleadosService) {
+    // this.empleados= this.empleadosService.empleados;
+
+  }
+
+  ngOnInit(): void {
+    this.empleados= this.empleadosService.empleados;
+  }
+
+  empleados:Empleado[]=[];
+
+  cuadroNombre:string="";
+  cuadroApellido:string="";
+  cuadroCargo:string="";
+  cuadroSalario:number=0;
+
+  agregarEmpleado() {
+    let miEmpleado=new Empleado(this.cuadroNombre, this.cuadroApellido, this.cuadroCargo, this.cuadroSalario);
+
+    //this.miServicio.muestraMensaje("Nombre del empleado" + miEmpleado.nombre);
+
+    this.empleadosService.agregarEmpleadoServicio(miEmpleado);
+  }
+}
